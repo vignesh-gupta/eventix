5aa6e7cf1a99ec2d1346aae100a8f250ea75e6cf
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-02-14 13:02:05 +0530

    (init) : setup convex + shadcn/ui

---
+++ convex/_generated/auth.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://accounts.google.com/",
+      applicationID: "your-application-id",
+    },
+  ],
+};
